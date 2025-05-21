73ef5d610898ce33c22011ae71b84009dc07d8d7
Jonathan Ilano <jonathan.ilano@gmail.com>
2025-05-21 10:18:47 +0800

    feat(lsp): add `biome_lint` function and keymap for linting
    - Introduced `biome_lint()` function to run Biome linting via shell command.
    - Mapped `<leader>cl` to `biome_lint()` for easier access.
    - Updated `lazy-lock.json` with new plugin commit hashes for fzf-lua and lualine.nvim.

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -1,5 +1,10 @@
 local Lsp = require "utils.lsp"
 
+function _G.biome_lint()
+  -- NOTE: Migrate to LSP later if it's available
+  local file = vim.fn.fnameescape(vim.fn.expand "%:p") -- Escape file path for shell
+  vim.cmd("!./node_modules/.bin/biome lint " .. file)
+end
 function _G.biome_fix()
   -- NOTE: Migrate to LSP later if it's available
   local file = vim.fn.fnameescape(vim.fn.expand "%:p") -- Escape file path for shell
@@ -17,6 +22,13 @@ return {
     Lsp.on_attach(client, bufnr)
     -- Map <leader>cb to biome_fix and <leader>cB to biome_fix_unsafe
     if client then
+      vim.api.nvim_buf_set_keymap(
+        bufnr,
+        "n",
+        "<leader>cl",
+        "<cmd>lua biome_lint()<CR>",
+        { noremap = true, silent = true, desc = "Biome: Lint" }
+      )
       vim.api.nvim_buf_set_keymap(
         bufnr,
         "n",
